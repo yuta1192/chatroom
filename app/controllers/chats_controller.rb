63f9a33595f406fa5d92cb9be0cda9aa384a5461
yuta <yuta.s1182@gmail.com>
2019-05-18 17:36:48 +0900

    devise add sitara message not view

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,11 @@
 class ChatsController < ApplicationController
   def index
     @messages = Message.all
+    @message = Message.new
   end
+
+  private
+    def message_params
+      params.require(:message).permit(:username, :content, :user)
+    end
 end
