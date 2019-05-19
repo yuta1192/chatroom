2cf0c67559d145f9f9435775e73c0083160e5d10
yuta <yuta.s1182@gmail.com>
2019-05-19 19:23:41 +0900

    user devise validate add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :authentication_keys => [:login]
+  devise :database_authenticatable, :registerable, :trackable, :confirmable,
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:login],
+         :omniauthable, omniauth_providers: [:twitter]
   has_many :messages, dependent: :destroy
   validates :username, presence: true
   attr_accessor :login
