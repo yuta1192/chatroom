f3d2b0db24633517cce96b4318ff4a5ca82d5504
yuta <yuta.s1182@gmail.com>
2019-05-19 18:56:18 +0900

    message model validate

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   after_create_commit { MessageBroadcastJob.perform_later self }
   belongs_to :user
+  validates :content, length: { minimum: 1 }
+  validates :content, presence: true
 end
