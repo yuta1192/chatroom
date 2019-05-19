5ea8b8643c9b1ade66c90d763dec047a3dac8abe
yuta <yuta.s1182@gmail.com>
2019-05-19 18:38:40 +0900

    avatar permit application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :avatar])
     end
 end
