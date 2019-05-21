b057f3a7d3c8610b4f3d874a1fb563a6833e54fd
yuta <yuta.s1182@gmail.com>
2019-05-21 23:37:15 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'chats#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  get '/users/auth/:provider/callback', to: 'sessions#create'
 
 # => if overridden routes generated controllers routes.rb
 #  devise_for :users, controllers: {
