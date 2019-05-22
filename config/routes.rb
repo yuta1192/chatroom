2d1de66d456ddddde4b8cb5caa8b770e0da8efd0
yuta <yuta.s1182@gmail.com>
2019-05-22 23:16:29 +0900

    パスワード変更用ページ作りたかった

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'chats#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users, :only => [:show]
 
 # => if overridden routes generated controllers routes.rb
 #  devise_for :users, controllers: {
