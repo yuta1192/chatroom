64aafc257183d5211d050777f5b475b7c97f4d84
yuta <yuta.s1182@gmail.com>
2019-05-26 14:01:24 +0900

    どうすれば自分だけ右になるのかわからん

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,9 +1,5 @@
 class ChatsController < ApplicationController
   def index
     @messages = Message.all
-    @u_message = Message.find_by(id: current_user)
-
-    not_current_user = User.where.not(id: current_user.id)
-    @other_message = Message.find_by(id: not_current_user)
   end
 end
